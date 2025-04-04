cac133b2a2cc699fac26def6673c833e774e408b
Garrett Wollman <wollman@csail.mit.edu>
2025-04-04 14:31:39 -0400

    FirehoseSubscriptionBase: update cursor db less frequently

--- src/util/subscription.ts
+++ src/util/subscription.ts
@@ -42,8 +42,8 @@ export abstract class FirehoseSubscriptionBase {
         this.handleEvent(evt).catch((err) => {
           console.error('repo subscription could not handle message', err)
         })
-        // update stored cursor every 20 events or so
-        if (isCommit(evt) && evt.seq % 20 === 0) {
+        // update stored cursor every 200 events or so
+        if (isCommit(evt) && evt.seq % 200 === 0) {
           await this.updateCursor(evt.seq)
         }
       }
